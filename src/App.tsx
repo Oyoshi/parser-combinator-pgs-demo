b1c008c991412296417e422b347673e91432b3fb
Oyoshi <rafal.apriasz@gmail.com>
2022-05-16 21:55:38 +0200

    feat: initialize project with React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SCHeader = styled.header`
+  text-align: center;
+  background: yellow;
+`;
+
+const App = () => (
+  <SCHeader>
+    <h1>Hello World</h1>
+  </SCHeader>
+);
+
+export default App;
