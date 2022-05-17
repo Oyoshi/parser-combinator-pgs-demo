93abc4f23108aef470b077749ed76809f13a4a54
Oyoshi <rafal.apriasz@gmail.com>
2022-05-17 09:26:20 +0200

    feat: handle input change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, ChangeEventHandler } from "react";
 import Layout from "components/layout";
 import Typography from "components/typography";
 import Input from "components/input";
@@ -7,40 +8,54 @@ import { log } from "utils/logger";
 const { Container, Header, Section } = Layout;
 const { Title, Text, Code } = Typography;
 
-const App = () => (
-  <Container>
-    <Header>
-      <Title level="1">Parser Combinator in TypeScript</Title>
-    </Header>
-    <Section>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Text>
-      <Code>{"<phoneNumber> ::= <fullNumber> | <basicNumber>"}</Code>
-      <Code>{"<fullNumber> ::= <areaCode> <basicNumber>"}</Code>
-      <Code>
-        {"<basicNumber> ::= <spaces> <fourDigits> <spaces> <fourDigits>"}
-      </Code>
-      <Code>{"<fourDigits> ::= <digit> <digit> <digit> <digit>"}</Code>
-      <Code>{'<areaCode> ::= "(" <digit> <digit> ")"'}</Code>
-      <Code>
-        {
-          '<digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"'
-        }
-      </Code>
-      <Code>{'<spaces> ::= " " <spaces> | ""'}</Code>
-    </Section>
-    <Section>
-      <Input />
-      <Button onClick={() => log("hello", "there")}>Parse</Button>
-    </Section>
-  </Container>
-);
+const App = () => {
+  const [parserInput, setParserInput] = useState<string>("");
+  const [parserResult, setParserResult] = useState<string>("");
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setParserInput(e.target.value);
+
+  // const handleButtonClick = () => pipe(parse, setParserResult);
+
+  return (
+    <Container>
+      <Header>
+        <Title level="1">Parser Combinator in TypeScript</Title>
+      </Header>
+      <Section>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Code>{"<phoneNumber> ::= <fullNumber> | <basicNumber>"}</Code>
+        <Code>{"<fullNumber> ::= <areaCode> <basicNumber>"}</Code>
+        <Code>
+          {"<basicNumber> ::= <spaces> <fourDigits> <spaces> <fourDigits>"}
+        </Code>
+        <Code>{"<fourDigits> ::= <digit> <digit> <digit> <digit>"}</Code>
+        <Code>{'<areaCode> ::= "(" <digit> <digit> ")"'}</Code>
+        <Code>
+          {
+            '<digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"'
+          }
+        </Code>
+        <Code>{'<spaces> ::= " " <spaces> | ""'}</Code>
+      </Section>
+      <Section>
+        <Input onChange={handleInputChange} />
+        <Button onClick={() => log("hello", "there")}>Parse</Button>
+      </Section>
+      <Section>
+        <Title level="2">Parser Result</Title>
+        <Text>{parserResult}</Text>
+      </Section>
+    </Container>
+  );
+};
 
 export default App;
--- src/components/layout/layout.style.ts
+++ src/components/layout/layout.style.ts
@@ -16,4 +16,5 @@ export const Container = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   width: 50%;
+  height: 50%;
 `;
